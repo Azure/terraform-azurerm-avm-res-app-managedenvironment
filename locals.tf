3a0e55c2aae41e41a3a8742e331c3197fc90f3ae
kewalaka <kewalaka@gmail.com>
2024-02-05 09:23:56 +1300

    fix: output format to deal with optional elements

--- locals.tf
+++ locals.tf
@@ -1,13 +1,13 @@
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 
-  workload_profile_outputs = {
+  workload_profile_outputs = jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles ? {
     for workload_profile, wp in jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles :
     workload_profile => {
       name                  = wp.name
       workload_profile_type = wp.workloadProfileType
-      maximum_count         = wp.maximumCount
-      minimum_count         = wp.minimumCount
+      maximum_count         = try(wp.maximumCount, null)
+      minimum_count         = try(wp.minimumCount, null)
     }
-  }
+  } : {}
 }
