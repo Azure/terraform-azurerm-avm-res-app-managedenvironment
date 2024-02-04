77679176d4f791f84441d78bc6605a61a9558fb7
kewalaka <kewalaka@gmail.com>
2024-02-04 15:00:33 +1300

    feat: adjust outputs to align with azurerm specification

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,20 @@
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
+locals {
+  resource_json_decoded = jsondecode(azapi_resource.this_environment)
+}
+
 output "resource" {
   description = "The Container Apps Managed Environment resource."
-  value       = azapi_resource.this_environment
+  value = {
+    # azapi_resource.this_environment
+    id = azapi_resource.this_environment.id
+    #default_domain
+    docker_bridge_cidr               = local.resource_json_decoded.properties.vnetConfiguration.dockerBridgeCidr
+    platform_reserved_cidr           = local.resource_json_decoded.properties.vnetConfiguration.platformReservedCidr
+    platform_reserved_dns_ip_address = local.resource_json_decoded.properties.vnetConfiguration.platformReservedDnsIP
+    #static_ip_address 
+  }
 }
 
 output "id" {
