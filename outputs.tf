72f12a17e125e7ef82ffb100590507899894f2a0
kewalaka <kewalaka@gmail.com>
2024-02-04 15:32:01 +1300

    fix: stop trying to be fancy with the jsondecode

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,5 @@
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
-# tflint-ignore: terraform_output_separate
-locals {
-  resource_json_decoded = jsondecode(azapi_resource.this_environment)
-}
 
 output "resource" {
   description = "The Container Apps Managed Environment resource."
@@ -11,9 +7,9 @@ output "resource" {
     # azapi_resource.this_environment
     id = azapi_resource.this_environment.id
     #default_domain
-    docker_bridge_cidr               = local.resource_json_decoded.properties.vnetConfiguration.dockerBridgeCidr
-    platform_reserved_cidr           = local.resource_json_decoded.properties.vnetConfiguration.platformReservedCidr
-    platform_reserved_dns_ip_address = local.resource_json_decoded.properties.vnetConfiguration.platformReservedDnsIP
+    docker_bridge_cidr               = jsondecode(azapi_resource.this_environment).properties.vnetConfiguration.dockerBridgeCidr
+    platform_reserved_cidr           = jsondecode(azapi_resource.this_environment).properties.vnetConfiguration.platformReservedCidr
+    platform_reserved_dns_ip_address = jsondecode(azapi_resource.this_environment).properties.vnetConfiguration.platformReservedDnsIP
     #static_ip_address 
   }
 }
