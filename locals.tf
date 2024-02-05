d56a95b9b7021b1574ea6b04163e9ed5860c6ebf
kewalaka <kewalaka@gmail.com>
2024-02-05 15:56:27 +1300

    fix: output format for workload profiles

--- locals.tf
+++ locals.tf
@@ -1,21 +1,19 @@
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 
-  workload_profile_outputs = jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles != null ? {
-    workloadProfiles = [
-      for wp in jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles : merge(
-        {
-          name                = wp.name
-          workloadProfileType = wp.workloadProfileType
-        },
-        // Only include minimumCount and maximumCount if workloadProfileType is not "Consumption"
-        wp.workloadProfileType != "Consumption" ? {
-          minimumCount = wp.minimumCount
-          maximumCount = wp.maximumCount
-        } : {}
-      )
-    ]
-  } : {}
+  workload_profile_outputs = jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles != null ? [
+    for wp in jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles : merge(
+      {
+        name                  = wp.name
+        workload_profile_type = wp.workloadProfileType
+      },
+      // Only include minimumCount and maximumCount if workloadProfileType is not "Consumption"
+      wp.workloadProfileType != "Consumption" ? {
+        minimum_count = wp.minimumCount
+        maximum_count = wp.maximumCount
+      } : {}
+    )
+  ] : null
 }
 
 
