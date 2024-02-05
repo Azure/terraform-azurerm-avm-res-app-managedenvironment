242cd47eb6fea98a14b981f944128778580879af
kewalaka <kewalaka@gmail.com>
2024-02-05 15:09:51 +1300

    fix: updates to outputs

--- locals.tf
+++ locals.tf
@@ -2,12 +2,20 @@ locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 
   workload_profile_outputs = jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles != null ? {
-    for workload_profile, wp in jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles :
-    workload_profile => {
-      name                  = wp.name
-      workload_profile_type = wp.workloadProfileType
-      maximum_count         = try(wp.maximumCount, null)
-      minimum_count         = try(wp.minimumCount, null)
-    }
+    workloadProfiles = [
+      for wp in jsondecode(data.azapi_resource.this_environment.output).properties.workloadProfiles : merge(
+        {
+          name                = wp.name
+          workloadProfileType = wp.workloadProfileType
+        },
+        // Only include minimumCount and maximumCount if workloadProfileType is not "Consumption"
+        wp.workloadProfileType != "Consumption" ? {
+          minimumCount = wp.minimumCount
+          maximumCount = wp.maximumCount
+        } : {}
+      )
+    ]
   } : {}
 }
+
+
--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,12 @@ output "resource" {
     resource_group_name = data.azurerm_resource_group.parent.name
     location            = jsondecode(data.azapi_resource.this_environment.output).location
 
+    # outputs provided by the AzureRM provider
     dapr_application_insights_connection_string = try(jsondecode(data.azapi_resource.this_environment.output).properties.daprAIInstrumentationKey, null)
     infrastructure_subnet_id                    = try(jsondecode(data.azapi_resource.this_environment.output).properties.vnetConfiguration.infrastructureSubnetId, null)
     internal_load_balancer_enabled              = try(jsondecode(data.azapi_resource.this_environment.output).properties.vnetConfiguration.internal, null)
     zone_redundancy_enabled                     = try(jsondecode(data.azapi_resource.this_environment.output).properties.zoneRedundant, null)
-    log_analytics_workspace_id                  = try(jsondecode(data.azapi_resource.this_environment.output).properties.AppLogsConfiguration.logAnalyticsConfiguration.customerId, null)
+    log_analytics_workspace_id                  = try(jsondecode(data.azapi_resource.this_environment.output).properties.appLogsConfiguration.logAnalyticsConfiguration.customerId, null)
     tags                                        = try(azapi_resource.this_environment.tags, null)
     workload_profiles                           = local.workload_profile_outputs
 
@@ -22,6 +23,12 @@ output "resource" {
     platform_reserved_cidr           = try(jsondecode(data.azapi_resource.this_environment.output).properties.vnetConfiguration.platformReservedCidr, null)
     platform_reserved_dns_ip_address = try(jsondecode(data.azapi_resource.this_environment.output).properties.vnetConfiguration.platformReservedDnsIP, null)
     static_ip_address                = jsondecode(data.azapi_resource.this_environment.output).properties.staticIp
+
+    # additional outputs not yet supported by the AzureRM provider
+    custom_domain_verification_id = try(jsondecode(data.azapi_resource.this_environment.output).properties.customDomainConfiguration.customDomainVerificationId, null)
+    mtls_enabled                  = try(jsondecode(data.azapi_resource.this_environment.output).properties.peerAuthentication.mtls.enabled, false)
+    infrastructure_resource_group = try(jsondecode(data.azapi_resource.this_environment.output).properties.infrastructureResourceGroup, null)
+
   }
 }
 
