a117ca6372fe34e3501c58d478a2b56bdfa02f6b
kewalaka <kewalaka@gmail.com>
2024-03-12 16:03:34 +1300

    fix: add prevent deletion block to provider block in examples

--- examples/workload_profile/main.tf
+++ examples/workload_profile/main.tf
@@ -10,7 +10,11 @@ terraform {
 
 provider "azurerm" {
   skip_provider_registration = true
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 # This ensures we have unique CAF compliant names for our resources.
